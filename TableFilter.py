74b9ff4cb68d99365511228d0e86afeac3bf28cf
J. Alexander <xandermoorman@gmail.com>
2017-05-02 23:12:40 -0500

    Create TableFilter.py

---
+++ TableFilter.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+(@)Author: glassCodeBender
+(#)Title: TableFilter.py
+(#)Version: 1.0
+
+Program Purpose: Experiment with filtering csv files.
+
+NEED TO ADD __main__ == __name__ and help functionality. 
+
+This program will be periodically updated to include much more functionality.
+
+"""
+
+import pandas as pd
+
+class TableFilter(object):
+    def __init__(self, file='', duplicate_column_name='', file_destination = ''):
+        set.__file1 = file
+        set.__duplicate = duplicate_column_name
+        set.__file_dest = file_destination
+
+    """
+        Description: Method filters out the unique values in the column of a csv file.
+        Return: DataFrame excluding the value
+    """
+    def populate_df(self):
+        try:
+            csv_file = self.__file1
+            df = pd.DataFrame()
+            pop_df = df.from_csv(csv_file)
+        except IOError as e:
+            print( "I/O error: The file did not import properly." )
+        return pop_df
+
+        """        
+        Create a column of boolean values 
+        Check to see if the previous value in the column is equal
+        """
+    def filter_df(self):
+        dup = self.__duplicate
+        pop_df = self.populate_df
+        pop_df['Unique'] = ( pop_df[dup] == pop_df[dup].shift() )
+        filtered_df = pop_df[ pop_df['Unique'] == False ]
+        return filtered_df
+
+    """ Export to filtered DataFrame to CSV file. """
+    def export_to_CSV(self):
+        fileDestination = self.__file_dest
+        df = self.filterDataFrame()
+        df.to_csv(fileDestination)
+        """
+        if __main__ == __name__:
+        """
+
+
