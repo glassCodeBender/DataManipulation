752a3d3522bc2b4f03e4cdae4247c93ef1b12b87
J. Alexander <xandermoorman@gmail.com>
2017-05-07 11:05:07 -0500

    Update tablefilter.py

--- tablefilter.py
+++ tablefilter.py
@@ -7,8 +7,9 @@
 Message me if you want me to add any functionality to the program, if you'd like me to write a subclass of the program,
 or if you want to hire me for a job. I also know Scala and Java pretty well. 
 
-WARNING: The program does not currently work from the commandline. However, the main aspects of the program work. I already
-used them to filter out duplicate names from an excel document. Also, the --compare functionality has not been added yet.
+WARNING: The program does not currently work from the commandline. However, the main aspects of the program work. 
+I already used them to filter out duplicate names from an excel document. Also, the --compare functionality has 
+not been added yet.
 
 Program Purpose: Program allows users to remove duplicate values from a column in a csv file (or from an Excel document 
 that was converted to a csv). The actual purpose of the program was to remove duplicate rows from an excel table so that 
@@ -32,8 +33,8 @@ import os
 import sys
 
 class TableFilter(object):
-
-    def __init__(self, file18 = '', column_name = '', file_destination = '', comparison_op = '', comparison_value = '', rm_duplicates = False ):
+    def __init__(self, file18='', column_name='', file_destination='', comparison_op='', comparison_value='',
+                 rm_duplicates=False):
         self.__file = file18
         self.__column = column_name
         self.__file_dest = file_destination
@@ -50,7 +51,7 @@ class TableFilter(object):
             df = pd.DataFrame()
             pop_df = df.from_csv(csv_file)
         except IOError as e:
-            print( "I/O error: The file did not import properly." )
+            print("I/O error: The file did not import properly.")
         return pop_df
 
         """        
@@ -59,6 +60,7 @@ class TableFilter(object):
         """
 
     def filter_uniq(self):
+        rm_duplicates = self.__rmdup
         assert rm_duplicates
 
         dup = self.__column
@@ -75,7 +77,7 @@ class TableFilter(object):
         comparison_value = self.__comp_value
         column = self.__column
         pop_df = self.populate_df()
-        
+
         # compare number values
         if isinstance(comparison_value, int) or isinstance(comparison_value, float):
             if comparison_op == 'EQ' or comparison_op == 'NEQ' or comparison_op == 'LTE' or comparison_op == 'GTE' or comparison_op == 'LT' or comparison_op == 'GT':
@@ -92,8 +94,9 @@ class TableFilter(object):
                 elif comparison_op == 'GT':
                     df = pop_df[pop_df[column] > comparison_value]
                 else:
-                    raise IOError("An exception was raised because you have deep psychological issues that affected your ability to use commandline tools.\n"
-                                  "\n\tOnly 'EQ', 'NEQ', 'LTE', 'GTE', 'LT', or 'GT' can be used as operators.")
+                    raise IOError(
+                        "An exception was raised because you have deep psychological issues that affected your ability to use commandline tools.\n"
+                        "\n\tOnly 'EQ', 'NEQ', 'LTE', 'GTE', 'LT', or 'GT' can be used as operators.")
         # compare string values       
         if isinstance(comparison_value, str):
             if comparison_op == 'EQ':
@@ -144,7 +147,7 @@ class TableFilter(object):
                     'An error occurred because you have deep psychological issues that affected your ability to use commandline tools.\n'
                     '\n\tONLY "EQ" and "NEQ" operations can performed when comparing values that include letters.')
         return df
- 
+
     """ Export to filtered DataFrame to CSV file. """
 
     def export_to_CSV(self):
@@ -153,9 +156,10 @@ class TableFilter(object):
         response = ''
 
         if rm_dup and comp_op and comp_val:
-            response = input('You indicated that you want to both exclude duplicate values from a column and filter the column by a value. Are you sure you want to do this? '
-                  '\n\nEnter "y" to export two different CSV files, "b" to both remove duplicates and filter, "n" to close the program, "d" to remove duplicates, or "f" to filter by the value entered: ')
-            
+            response = input(
+                'You indicated that you want to both exclude duplicate values from a column and filter the column by a value. Are you sure you want to do this? '
+                '\n\nEnter "y" to export two different CSV files, "b" to both remove duplicates and filter, "n" to close the program, "d" to remove duplicates, or "f" to filter by the value entered: ')
+
             if response.upper() == 'Y':
                 df = self.filter_uniq()
                 df1 = self.filter_by()
@@ -168,45 +172,53 @@ class TableFilter(object):
             elif response.upper() == 'F':
                 df = self.filter_by()
             else:
-                raise IOError ("Something when wrong while attempting to export the formatted data to a CSV file.")
+                raise IOError("Something when wrong while attempting to export the formatted data to a CSV file.")
 
         if rm_dup and comp_op and comp_val and response == 'Y':
-
             df.to_csv(file_destination)
             df1.to_csv(file_destination + 'filtered')
-            print( 'You indicated that you both want to exclude duplicate values from a column and filter a column by a value. \n'
-                   'We output the filtered column to ' + file_destination + 'filtered' )
+            
+            print(
+            'You indicated that you both want to exclude duplicate values from a column and filter a column by a value. \n'
+            'We output the filtered column to ' + file_destination + 'filtered')
             sys.exit(0)
         else:
             df.to_csv(file_destination)
-            
+
     """ Process command-line arguments. """
     if __name__ == '__main__':
 
         """ Add commandline help functionality to the program """
-        parser = argparse.ArgumentParser( add_help = True, description = "Allows users to filter CSV file in a variety of ways." )
+        parser = argparse.ArgumentParser(add_help = True,
+                                         description = "Allows users to filter a CSV file in a variety of ways.")
 
         # NEED TO ADD PARSER GROUP CALLED 'Positional Arguments'
         # parse.add_argument_group()
-        parser.add_argument( '-f', '--file', dest = 'store', dest = 'file', help='Store the name of the csv file you want converted' )
-        parser.add_argument( '-n', '--column', action = 'store', dest = 'column', help='Store the name of the column you want filtered.' )
-        parser.add_argument( '-r', '--rmdup', action = 'store_true', dest = 'rmdup', help = 'If -r is added to your command, the program will remove all duplicates of the value passed to --column.')
+        parser.add_argument('-f', '--file', action='store', dest = 'file',
+                            help = 'Store the name of the csv file you want converted')
+        parser.add_argument('-n', '--column', action='store', dest = 'column',
+                            help ='Store the name of the column you want filtered.')
+        parser.add_argument('-r', '--rmdup', action='store_true', dest = 'rmdup',
+                            help = 'If -r is added to your command, the program will remove all duplicates of the value passed to --column.')
         # NEED TO ADD PARSER GROUP CALLED 'Optional Arguments'
         # parser.add_argument_group()
-        parser.add_argument( '-d', '--dest', action = 'store', dest = 'file_destination', default= str( os.getcwd() ) + "/filteredcsvfile.csv",
-                                                                                          help = "Store the name of the file you'd like the program to create" )
-        parser.add_argument( '-c', '--compare', action = 'store', dest = 'compare', help = 'Command allows user to filter a column based on an operator passed to -c and '
-                                                                                           'a value (String or Float) passed to -z. \n'
-                                                                                           'Accepted Values: \n\t"gt" for greater than, '
-                                                                                           '\n"lt" for less than, '
-                                                                                           '\n"ne" for not equal" '
-                                                                                            '\n"lte" for less than or equal'
-                                                                                           '\n"gte" for greater than or equal to'
-                                                                                            '\n"eq" for equals' )
-        parser.add_argument( '-z', '--value', action = 'store', dest = 'comp_value', help = 'Enter a value that you want to filter a column by. \n'
-                                                                                            'For example: The following command will filter out all rows where "Gross Sales" was less than 30'
-                                                                                            '\n~$ python tablefilter.py -f myfile.csv -n "Gross Sales" -c lt -z 30')
-        parser.add_argument( '-v', '--verbose', action = 'store_true', help = 'Increase the verbosity of the program.' )
+        parser.add_argument('-d', '--dest', action='store', dest='file_destination',
+                            default = str(os.getcwd()) + "/filteredcsvfile.csv",
+                            help = "Store the name of the file you'd like the program to create")
+        parser.add_argument('-c', '--compare', action = 'store', dest = 'compare',
+                            help = 'Command allows user to filter a column based on an operator passed to -c and '
+                                 'a value (String or Float) passed to -z. \n'
+                                 'Accepted Values: \n\t"gt" for greater than, '
+                                 '\n"lt" for less than, '
+                                 '\n"ne" for not equal" '
+                                 '\n"lte" for less than or equal'
+                                 '\n"gte" for greater than or equal to'
+                                 '\n"eq" for equals')
+        parser.add_argument('-z', '--value', action='store', dest='comp_value',
+                            help = 'Enter a value that you want to filter a column by. \n'
+                                 'For example: The following command will filter out all rows where "Gross Sales" was less than 30'
+                                 '\n~$ python tablefilter.py -f myfile.csv -n "Gross Sales" -c lt -z 30')
+        parser.add_argument('-v', '--verbose', action = 'store_true', help = 'Increase the verbosity of the program.')
 
         if len(sys.argv) <= 2:
             parser.print_help()
@@ -217,17 +229,17 @@ class TableFilter(object):
         file = args.file
         file_dest = args.file_destination
         column = args.column
-        compare = args.compare 
+        compare = args.compare
         rmdup = args.rmdup
-        
-        assert os.path.exists( str(os.getcwd()) + '/' + file )
 
-        filter_object = TableFilter( file, column, file_dest, compare, rmdup)
+        assert os.path.exists(str(os.getcwd()) + '/' + file)
+
+        filter_object = TableFilter(file, column, file_dest, compare, rmdup)
         filter_object.export_to_CSV()
 
         if args.verbose:
-            assert isinstance( file_dest )
-            print( 'Your csv file has been filtered and saved to %s' % file_dest )
+            assert isinstance(file_dest)
+            print('Your csv file has been filtered and saved to %s' % file_dest)
 
         """
         if file_destination is None:
